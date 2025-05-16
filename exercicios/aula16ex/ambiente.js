68920ef663c19f65a076254e6ea129a4fcf13da4
Miguel Santos <miguelsantosdev@gmail.com>
2025-05-16 03:25:30 -0300

    commit JS update 1.5

---
+++ exercicios/aula16ex/ambiente.js
@@ -0,0 +1,17 @@
+/*while(contador <= 6){
+    console.log('Tudo bem')
+    contador++
+}
+*/
+
+/*do{
+    console.log(`Passo ${contador}`)
+    contador++
+} while(contador <= 6)
+
+*/
+console.log('Chegou a hora de caminhar ...')
+for (var contador = 1; contador <=10; contador++){
+    console.log (`Passo ${contador}`)
+}
+console.log('Você chegou ao seu Destino !')
