bf33355c4abd68a34a2e019f7e5f76e006831b03
Miguel Santos <miguelsantosdev@gmail.com>
2025-06-10 00:27:04 -0300

    commit contador update

--- aula17-exercicios/aula13ex/script.js
+++ aula17-exercicios/aula13ex/script.js
@@ -1,6 +1,11 @@
 var contador = 0
 
-while (contador <= 6) {
+/*do {
     console.log(`${contador}`)
     contador++
-}
+} while (contador <= 6) */
+
+/*         for(inicio; teste; incremento)       */
+for (contador = 0; contador  <= 20; contador++){
+    console.log(` -> ${contador}`)
+}   
