306e08f10f7f2dacc58e14b22a79a8b7cf516af1
Miguel Santos <miguelsantosdev@gmail.com>
2025-06-23 00:30:57 -0300

    commit

--- exercicios/aula21ex/script.js
+++ exercicios/aula21ex/script.js
@@ -1,17 +1,25 @@
-let n = document.querySelector('input#anum')
-let listn = document.querySelector('select#numlist')
-let nres = document.querySelector('div#res')
-let valores = []
+let n = document.querySelector('input#anum') // pega o valor de anum que sera o numero que o usuario ira adicionar
 
-function isnum(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
-        return true
+let listn = document.querySelector('select#numlist') // pega a lista de numeros que o usuario inseriu representada por numlist
+
+let nres = document.querySelector('div#res') // pega o resultado da analise dos numeros inseridos (res)
+
+let valores = [] // representa o vetor com os indicies que irao conter os numeros para análise.
+
+function isnum(n){ //função isnum recebe o numero que o usuário digitar(n). Faça...
+
+    if(Number(n) >= 1 && Number(n) <= 100){ // Se numero(n) é  maior ou igual a 1 E(&&) numero for menor ou igual a 100, executa ...
+
+        return true // Verdadeiro. Valor é lido.
+
     } else {
-        return false
+
+        return false // Falso. Valor não lido.
     }
 }
 
-function inlistn(n, l){
+function inlistn(n, l){ // função nlista verifica um numero (n) dentro da lista (l).
+
     if(l.indexOf(Number(n)) != -1){
         return true
     } else {
@@ -19,19 +27,35 @@ function inlistn(n, l){
     }
 }
 
+// ! = NÃO
 
-function adicionar(){
-    if (isnum(n.value) && !inlistn(n.value, valores)){
-        window.alert('Valor inserido.')
+function adicionar(){ // função adiciona um numero ao usuario clicar em Adicionar.
+
+    if (isnum(n.value) && !inlistn(n.value, valores)){ // Se o numero lido NÃO(!) estiver na lista (inlistn), (n.value, valores[valores é o vetor]), faça...
+       
+         valores.push(Number(n.value, valores))// Adiciona(push) o valor inserido pelo usuário(n.value) dentro do vetor[valores].
+
+         let item = document.createElement('option') // Option é um elemento criado para escrever o valor na lista(dentro do vetor).E item é a variavel deste elemento. 
+
+         item.text = `Valor ${n.value} foi inserido.` // item.text faz com que escreva o valor dentro de item{n.value} como uma mensagem. 
+
+         listn.appendChild(item) // esta linha passa item como elemento filho da lista. appendChild(item). Faz com o elemento seja inserido na lista em definitivo e escrevendo a mensagem em definitivo.
+
+         alert('valor inserido') 
+
     } else {
-        window.alert('Valor inválido.')
-    } 
+        //window.alert('Valor inválido.') // Valor inválido. Valor incorreto
+        alert('valor repetido ou invalido.')
+    }
+    n.value = '' // atributo.value = '' faz com que sempre limpre a barra de digitação do usuário após enviar um valor.
+
+    n.focus() // atributo.focus() faz com que o cursor não saia da barra de digitação do usuario. 
+}
+
+function analisar(){
+    
 }
 
 
 
 
-
-/*function analise(n){
-
-} */
